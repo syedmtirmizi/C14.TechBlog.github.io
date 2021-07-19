f588fcb68de136d759497179bb4386ffcfef4806
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-07-18 22:29:35 -0400

    update jawsdb_url

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 require('dotenv').config({path: __dirname + '/.env'})
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = "mysql://vjuk7s1mhxsbddyr:kgufseqltpxgpv7m@s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j86mrzdgvzqkvcuy"
+  ? new Sequelize("mysql://vjuk7s1mhxsbddyr:kgufseqltpxgpv7m@s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j86mrzdgvzqkvcuy")
   : new Sequelize("techblog_db", "root", "Pakistan0412!", {
     host: 'localhost',
     dialect: 'mysql',
