eaa134376f37560099d32fa8fef77dd2a4fe7b76
Syed Tirmizi <syedmtirmizi@gmail.com>
2021-07-20 11:08:36 -0400

    update .env variables

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,13 @@ const path = require("path");
 
 require('dotenv').config({path: __dirname + '/.env'})
 
-const sequelize = "mysql://vjuk7s1mhxsbddyr:kgufseqltpxgpv7m@s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j86mrzdgvzqkvcuy"
-  ? new Sequelize("mysql://vjuk7s1mhxsbddyr:kgufseqltpxgpv7m@s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/j86mrzdgvzqkvcuy")
-  : new Sequelize("techblog_db", "root", "Pakistan0412!", {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     dialectOptions: {
       decimalNumbers: true,
     },
   });
-
 module.exports = sequelize;
